73231172dbff7d6c85c67d71deb760e3886e46ea
Ivan.Yakovlev <ivan.yakovlev@pochtavip.com>
2020-07-19 17:25:02 +0300

    IE fixes + header feature

---
+++ src/Containers/Main/index.tsx
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import './style.sass'
+
+
+export default class Main extends Component {
+    render(){
+        return (
+            <main className="main">
+
+            </main>
+        )
+    }
+} 
